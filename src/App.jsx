80c2f310fc0aa0f3ffeaa3ec637b164c2ddf4f2c
Victoria Azola Silva <116470398+VickyAzola@users.noreply.github.com>
2024-03-28 21:02:18 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,20 @@ function App() {
     <>
       <main>
       <div className='bg-color-purple'></div>
-        <div className='phone-Container'>
+        <div className='phone-Container scale-up-horizontal-left'>
           <Phone />
         </div>
-        <section className='title-section'>
+        <section className='title-section scale-up-horizontal-left'>
           <h1 className='title'>Simple booking</h1>
           <p className='description'>
             Stay in touch with our dog walkers through the chat interface. This makes it easy to 
             discuss arrangements and make bookings. Once the walk has been completed you can rate 
             your walker and book again all through the chat.
           </p>
+          <div className='bg-color-pink-mobile'></div>
           <Credits />
         </section>
-        <div className='bg-color-pink'></div>
+        <div className='bg-color-pink-desktop'></div>
       </main>
     </>
   )
